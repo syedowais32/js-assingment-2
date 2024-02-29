ee0e5b4ec0afb9b6071f52136dc9596b13f450ee
SyedGulzar786 <153593889+SyedGulzar786@users.noreply.github.com>
2024-02-29 14:33:08 +0500

    practice done

---
+++ app.js
@@ -0,0 +1,7 @@
+// just for practicing lecture
+// var a = 5 ;
+// var b = 7 ;
+// // console.log(a++ , --b , b++ , --a , a++)
+// var num = (a++ + --b) - b++ - --a * a++
+// alert(num) 
+// .... ended
